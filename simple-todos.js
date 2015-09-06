2cae407979ce91afb531ffea7036b3de1d486be4
yutayokoi <dezirus.novajn.iojn@gmail.com>
2015-09-06 15:28:12 +0900

    メニュー表示の為のセッション追加

--- simple-todos.js
+++ simple-todos.js
@@ -3,7 +3,17 @@ Tasks = new Mongo.Collection("tasks");
 if (Meteor.isClient) {
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({}, {sort: {createdAt: 1}});
+      if (Session.get("hideCompleted")) {
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+    hideCompleted: function () {
+      return Session.get("hideCompleted");
+    },
+    incompleteCount: function () {
+      return Tasks.find({checked: {$ne: true}}).count();
     }
   });
 
@@ -23,6 +33,9 @@ if (Meteor.isClient) {
 
       // Clear form
       event.target.text.value = "";
+    },
+    "change .hide-completed input": function (event) {
+      Session.set("hideCompleted", event.target.checked);
     }
   });
 
